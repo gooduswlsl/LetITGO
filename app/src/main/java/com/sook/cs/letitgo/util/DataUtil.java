55c99f1b661bef7396609c1f912e352215cedb5f
YeonJin <hongy3@naver.com>
2018-01-18 06:12:19 +0900

    update data

--- app/src/main/java/com/sook/cs/letitgo/util/DataUtil.java
+++ app/src/main/java/com/sook/cs/letitgo/util/DataUtil.java
@@ -12,6 +12,18 @@ public class DataUtil {
         storeList.add(new Store("할리스", "숙대점", "010-4536-9508", "www.naver.com", "서울특별시 용산구", "https://pbs.twimg.com/profile_images/1570768836/phpPj5DaU"));
         storeList.add(new Store("스타벅스", "죽전점", "010-4536-9508",
                 "www.daum.net", "경기도 용인시 수지구", "http://logok.org/wp-content/uploads/2014/08/Starbucks-logo-2000px-png.png"));
+        storeList.add(new Store("커피빈", "숙대점", "010-4536-9508", "www.naver.com", "서울특별시 용산구", "http://www.m-i.kr/news/photo/201505/165854_88460_2856.png"));
+        storeList.add(new Store("스타벅스", "죽전점", "010-4536-9508",
+                "www.daum.net", "경기도 용인시 수지구", "http://logok.org/wp-content/uploads/2014/08/Starbucks-logo-2000px-png.png"));
+        storeList.add(new Store("투썸", "서울역점", "010-4536-9508", "www.naver.com", "서울특별시 용산구", "https://pbs.twimg.com/profile_images/1366417558/twosomenowon.png_400x400.png"));
+        storeList.add(new Store("스타벅스", "죽전점", "010-4536-9508",
+                "www.daum.net", "경기도 용인시 수지구", "http://logok.org/wp-content/uploads/2014/08/Starbucks-logo-2000px-png.png"));
+        storeList.add(new Store("커피니", "숙대점", "010-4536-9508", "www.naver.com", "서울특별시 용산구", "https://pbs.twimg.com/profile_images/1570768836/phpPj5DaU"));
+        storeList.add(new Store("놀숲", "죽전점", "010-4536-9508",
+                "www.daum.net", "경기도 용인시 수지구", "http://logok.org/wp-content/uploads/2014/08/Starbucks-logo-2000px-png.png"));
+        storeList.add(new Store("할리스", "용산점", "010-4536-9508", "www.naver.com", "서울특별시 용산구", "https://pbs.twimg.com/profile_images/1570768836/phpPj5DaU"));
+        storeList.add(new Store("쌀국수", "죽전점", "010-4536-9508",
+                "www.daum.net", "경기도 용인시 수지구", "http://logok.org/wp-content/uploads/2014/08/Starbucks-logo-2000px-png.png"));
         return storeList;
     }
 
@@ -19,6 +31,13 @@ public class DataUtil {
         ArrayList<Menu> menuList = new ArrayList<>();
         menuList.add(new Menu("아메리카노", "http://www.rotiboy.kr/uploads/menu/Dio548fuq13CkrX6.png", 5000, "맛있어요", "할리스", "숙대점"));
         menuList.add(new Menu("카페라떼", "http://www.dunkindonuts.co.kr/upload/product/big_824.png", 5500, "맛나요", "스타벅스", "죽전점"));
+        menuList.add(new Menu("고구마라떼", "http://www.rotiboy.kr/uploads/menu/Dio548fuq13CkrX6.png", 5000, "맛있어요", "커피니", "숙대점"));
+        menuList.add(new Menu("바닐라라떼", "http://www.dunkindonuts.co.kr/upload/product/big_824.png", 5500, "맛나요", "놀숲", "죽전점"));
+        menuList.add(new Menu("자몽티", "http://www.rotiboy.kr/uploads/menu/Dio548fuq13CkrX6.png", 5000, "맛있어요", "투썸", "서울역점"));
+        menuList.add(new Menu("녹차", "http://www.dunkindonuts.co.kr/upload/product/big_824.png", 5500, "맛나요", "커피빈", "죽전점"));
+        menuList.add(new Menu("제주녹차", "http://www.rotiboy.kr/uploads/menu/Dio548fuq13CkrX6.png", 5000, "맛있어요", "할리스", "숙대점"));
+        menuList.add(new Menu("한라봉차", "http://www.dunkindonuts.co.kr/upload/product/big_824.png", 5500, "맛나요", "엔젤리너스", "죽전점"));
+        menuList.add(new Menu("유자차", "http://www.rotiboy.kr/uploads/menu/Dio548fuq13CkrX6.png", 5000, "맛있어요", "스타벅스", "숙대점"));
         return menuList;
     }
 }
