94fc1f1bf777928c9feaa6cceebde9b8a2abdd55
YeonJin <hongy3@naver.com>
2018-01-29 20:27:35 +0900

    update web

--- web/routes/member.js
+++ web/routes/member.js
@@ -8,7 +8,7 @@ router.get('/:phone', function(req, res, next) {
   var phone = req.params.phone;
   
   var sql = "select * " +
-            "from bestfood_member " + 
+            "from member " + 
             "where phone = ? limit 1;";  
   console.log("sql : " + sql);    
 	
@@ -28,11 +28,11 @@ router.post('/phone', function(req, res) {
   var phone = req.body.phone;
 
   var sql_count = "select count(*) as cnt " +
-            "from bestfood_member " + 
+            "from member " + 
             "where phone = ?;";  
   console.log("sql_count : " + sql_count);
 
-  var sql_insert = "insert into bestfood_member (phone) values(?);";
+  var sql_insert = "insert into member (phone) values(?);";
     
   db.get().query(sql_count, phone, function (err, rows) {
     console.log(rows);
@@ -53,25 +53,23 @@ router.post('/phone', function(req, res) {
 //member/info
 router.post('/info', function(req, res) {
   var phone = req.body.phone;
-  var name = req.body.name;
-  var sextype = req.body.sextype;
-  var birthday = req.body.birthday;
+  var group = req.body.group;
 
   console.log({name, sextype, birthday, phone});
 
   var sql_count = "select count(*) as cnt " +
-            "from bestfood_member " + 
+            "from member " + 
             "where phone = ?;";
 
-  var sql_insert = "insert into bestfood_member (phone, name, sextype, birthday) values(?, ?, ?, ?);";
-  var sql_update = "update bestfood_member set name = ?, sextype = ?, birthday = ? where phone = ?; ";
-  var sql_select = "select seq from bestfood_member where phone = ?; ";
+  var sql_insert = "insert into member (phone, group) values(?, ?);";
+  var sql_update = "update member set group = ? where phone = ?; ";
+  var sql_select = "select seq from member where phone = ?; ";
   
   db.get().query(sql_count, phone, function (err, rows) {
     if (rows[0].cnt > 0) {
       console.log("sql_update : " + sql_update);
 
-      db.get().query(sql_update, [name, sextype, birthday, phone], function (err, result) {
+      db.get().query(sql_update, [group, phone], function (err, result) {
         if (err) return res.sendStatus(400);
         console.log(result);
 
@@ -84,7 +82,7 @@ router.post('/info', function(req, res) {
     } else {
       console.log("sql_insert : " + sql_insert);
 
-      db.get().query(sql_insert, [phone, name, sextype, birthday], function (err, result) {
+      db.get().query(sql_insert, [phone, group], function (err, result) {
         if (err) return res.sendStatus(400);
 
         res.status(200).send('' + result.insertId);
@@ -92,23 +90,5 @@ router.post('/info', function(req, res) {
     }
   });
 });
-
-//member/icon_upload
-router.post('/icon_upload', function (req, res) {
-  var form = new formidable.IncomingForm();
-
-  form.on('fileBegin', function (name, file){
-    file.path = './public/member/' + file.name;
-  });
-
-  form.parse(req, function(err, fields, files) {
-    var sql_update = "update bestfood_member set member_icon_filename = ? where seq = ?;";
-
-    db.get().query(sql_update, [files.file.name, fields.member_seq], function (err, rows) {
-      res.sendStatus(200);
-    });
-  });
-});
-
 
 module.exports = router;
