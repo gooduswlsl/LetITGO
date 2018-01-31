b2b9e0ddce5f536a807d1c34a9f57eb85edc85c4
YeonJin <hongy3@naver.com>
2018-01-31 19:11:08 +0900

    remove seller.js

--- web/routes/member.js
+++ web/routes/member.js
@@ -28,10 +28,9 @@ router.get('/:phone', function(req, res, next) {
   });
 });
 
-//member/:customer_seq
+//member/customer/:customer_seq
 router.get('/customer/:customer_seq', function(req, res, next){
 	var seq = req.params.customer_seq;
-	//console.log("seq"+seqs);
 	var sql_select = "select * from customer where seq = ? limit 1;";
 	
 	db.get().query(sql_select, seq, function(err, rows){
@@ -80,6 +79,57 @@ router.post('/customer', function(req, res) {
 
     });
   });
+
+//member/seller
+router.post('/seller', function(req, res) {
+    var phone = req.body.phone;
+    var name = req.body.name;
+    var site = req.body.site;
+    var tel = req.body.tel;
+    var address = req.body.address;
+    var webpage = req.body.webpage;
+    //var img = req.body.img;
+
+  console.log({name,site,tel,address,webpage,phone});
+
+  var sql_count = "select count(*) as cnt " +
+            "from seller " + 
+            "where phone = ?;";
+
+  var sql_insert = "insert into seller (name, site, tel, address, webpage, phone) values(?, ?, ?, ?, ?, ?);";
+  var sql_update = "update seller set name = ?, site = ?, tel = ?, address = ?, webpage = ? where phone = ?; ";
+  var sql_select = "select seq from seller where phone = ?; ";
+  
+  db.get().query(sql_count, phone, function (err, rows) {
+    if (rows[0].cnt > 0) {
+      console.log("sql_update : " + sql_update);
+
+      db.get().query(sql_update, [name, site, tel, address, webpage, phone], function (err, result) {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(400);
+        }
+        console.log(result);
+
+        db.get().query(sql_select, phone, function (err, rows) {
+          if (err) return res.sendStatus(400);
+
+          res.status(200).send('' + rows[0].seq);
+        });
+      });
+    } else {
+      console.log("sql_insert : " + sql_insert);
+
+      db.get().query(sql_insert, [name,site,tel,address,webpage,phone], function (err, result) {
+        if (err) return res.sendStatus(400);
+
+        res.status(200).send('' + result.insertId);
+      });
+    }
+  });
+});
+
+
 
 
 //member/img_upload
